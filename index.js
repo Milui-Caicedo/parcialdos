4a2069d3632358d8c759a9d4cef634394d1f1d63
Luisa Maria Caicedo Benitez <1144068260@academico.icesi.edu.co>
2018-11-16 08:14:52 -0500

    aqui va

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static("public"));
+app.engine('handlebars',hbs());
+app.set('view-engine','handlebars');
+
+
+app.get("/", function(req,resp){
+    resp.render('home');
+});
+
+
+
+app.listen(5500);
